5c4b11da7b036747aec6cf53adf81d553a2e3eea
aemo63 <mcflurryisfet@gmail.com>
2024-11-16 18:19:14 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("reveal-btn").addEventListener("click", () => {
+  // Hide the initial text and button
+  document.getElementById("initial-text").style.display = "none";
+  
+  // Show the memories text
+  document.getElementById("memories-text").style.display = "block";
+});
